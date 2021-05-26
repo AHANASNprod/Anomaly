6863381c4778d377a5ed1c5f462f4d8f5fc55ff9
ADIGGEWA <ajaharikram.diggewadi@fujitsu.com>
2021-05-26 09:25:46 +0530

    updating

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express'),
        path = require('path');
 
 const app = express();
+app.use((req, res, next) => {
+       res.header("Access-Control-Allow-Origin","*");
+       res.header("Access-Control-Allow-Headers",
+                 "Origin,X-Requested-With,Content-Type,Accept,Authorization");
+       res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+       next();});
 app.listen(process.env.PORT || 8080, () =>
 {
 });
